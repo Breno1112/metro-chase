345a7f1218580d44b5e4243fce24fcf618ad8cb7
Breno1112 <contato.brenofachini@hotmail.com>
2022-07-27 18:59:29 -0300

    map generating by itself

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,16 +6,33 @@ public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private Transform starting_level_part;
     [SerializeField] private Transform levelPart1;
+
+    [SerializeField] private Transform player;
+
+    private const float DISTANCE_TO_SPAWN = 30f;
+    private Vector3 lastEndPosition;
     // Start is called before the first frame update
     private void Awake() {
-        Transform lastLevelTransform = SpawnLevelPart(starting_level_part.Find("EndPosition").position);
-        lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
-        lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
-        lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
-        lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
+        lastEndPosition = starting_level_part.Find("EndPosition").position;
+        // Transform lastLevelTransform = SpawnLevelPart(starting_level_part.Find("EndPosition").position);
+        // lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
+        // lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
+        // lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
+        // lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("EndPosition").position);
     }
 
-    private Transform SpawnLevelPart(Vector3 spawnPosition) {
+    private void Update() {
+        if (Vector3.Distance(player.position, lastEndPosition) < DISTANCE_TO_SPAWN) {
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart() {
+        Transform levelPart = CalculateAndSpawnLevelPart(lastEndPosition);
+        lastEndPosition = levelPart.Find("EndPosition").position;
+    }
+
+    private Transform CalculateAndSpawnLevelPart(Vector3 spawnPosition) {
         return Instantiate(levelPart1, new Vector3(spawnPosition.x + 10.75f, 0), Quaternion.identity);
     }
 }
